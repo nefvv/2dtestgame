b28159ae411b6e452fea11666e1796afcca4d8fb
nefvv <stevenschweigert6@gmail.com>
2023-10-29 14:30:12 +0100

    added death logic + traps

--- Assets/scripts/ItemCollector.cs
+++ Assets/scripts/ItemCollector.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
+    [SerializeField] private Text strawberriesText;
+    private int strawberries = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("strawberry"))
         {
             Destroy(collision.gameObject);
+            strawberries++;
+
+            strawberriesText.text = "Strawberries : " + strawberries;
         }
 
     }
